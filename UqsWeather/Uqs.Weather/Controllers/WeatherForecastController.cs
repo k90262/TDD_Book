21594068d6e777034d9e430c4ebbf4c5a423291d
Bill Ho <ycho.bill.lab@gmail.com>
2024-04-12 00:02:07 +0800

    [WeatherForecastController] (1. added function MapFeelToTemp, (2. [NuGet] install AdamTibi.OpenWeatherClient

--- UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
+++ UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
@@ -10,23 +10,35 @@ public class WeatherForecastController : ControllerBase
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
+    private static readonly int FORECAST_DAYS = 5;
     private readonly ILogger<WeatherForecastController> _logger;
+
+    private string MapFeelToTemp(int temperatureC)
+    {
+        if (temperatureC <= 0) return Summaries.First();
+        int summariesIndex = (temperatureC / 5) + 1;
+        if (summariesIndex >= Summaries.Length) return Summaries.Last();
+        return Summaries[summariesIndex];
+    }
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet(Name = "GetRandomWeatherForecast")]
+    public IEnumerable<WeatherForecast> GetRandom()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
+        return Enumerable.Range(1, FORECAST_DAYS).Select(index =>
+            {
+                var tempC = Random.Shared.Next(-20, 55);
+                return new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = tempC,
+                    Summary = MapFeelToTemp(tempC)
+                };
+            })
         .ToArray();
     }
 }
