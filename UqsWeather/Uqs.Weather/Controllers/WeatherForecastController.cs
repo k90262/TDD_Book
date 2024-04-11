ec2c9d9c4bc38ad9c096b7388591d27c544121b4
Bill Ho <ycho.bill.lab@gmail.com>
2024-04-12 01:17:03 +0800

    [WeatherForecastController] added GetReal api (call OpenWeatherMap api with personal api key) and ConvertCToF api

--- UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
+++ UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using AdamTibi.OpenWeather;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Uqs.Weather.Controllers;
@@ -6,12 +7,14 @@ namespace Uqs.Weather.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
+    private const int FORECAST_DAYS = 5;
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IConfiguration _config;
+    
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-    private static readonly int FORECAST_DAYS = 5;
-    private readonly ILogger<WeatherForecastController> _logger;
 
     private string MapFeelToTemp(int temperatureC)
     {
@@ -21,12 +24,21 @@ public class WeatherForecastController : ControllerBase
         return Summaries[summariesIndex];
     }
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration config)
     {
         _logger = logger;
+        _config = config;
     }
 
-    [HttpGet(Name = "GetRandomWeatherForecast")]
+    [HttpGet("ConvertCToF")]
+    public double ConvertCToF(double c)
+    {
+        double f = c * (9d / 5d) + 32;
+        _logger.LogInformation("conversion requested");
+        return f;
+    }
+
+    [HttpGet("GetRandomWeatherForecast")]
     public IEnumerable<WeatherForecast> GetRandom()
     {
         return Enumerable.Range(1, FORECAST_DAYS).Select(index =>
@@ -39,6 +51,33 @@ public class WeatherForecastController : ControllerBase
                     Summary = MapFeelToTemp(tempC)
                 };
             })
-        .ToArray();
+            .ToArray();
+    }
+    
+    
+    [HttpGet("GetRealWeatherForecast")]
+    public async Task<IEnumerable<WeatherForecast>> GetReal()
+    {
+        const decimal GREENWICH_LAT = 51.4810m;
+        const decimal GREENWICH_LON = 0.0052m;
+        string apiKey = _config["OpenWeather:Key"];
+        HttpClient httpClient = new HttpClient();
+        Client openWeatherClient = new Client(apiKey, httpClient);
+        OneCallResponse res = await openWeatherClient.OneCallAsync(GREENWICH_LAT, GREENWICH_LON, new []
+        {
+            Excludes.Current, Excludes.Minutely, Excludes.Hourly, Excludes.Alerts
+        }, Units.Metric);
+        return Enumerable.Range(1, FORECAST_DAYS).Select(index =>
+            {
+                double forecastedTemp = res.Daily[index].Temp.Day;
+                int tempC = (int)Math.Round(forecastedTemp);
+                return new WeatherForecast
+                {
+                    Date = res.Daily[index].Dt,
+                    TemperatureC = tempC,
+                    Summary = MapFeelToTemp(tempC)
+                };
+            })
+            .ToArray();
     }
 }
