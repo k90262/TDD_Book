b6db19d19df79e14f900bb0b3b4403b9a5c800ec
Bill Ho <ycho.bill.lab@gmail.com>
2024-04-13 00:19:17 +0800

    added ClientStub for load testing

--- UqsWeather/Uqs.Weather/Program.cs
+++ UqsWeather/Uqs.Weather/Program.cs
@@ -1,4 +1,5 @@
 using AdamTibi.OpenWeather;
+using Uqs.Weather;
 using Uqs.Weather.Wrappers;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,13 +8,18 @@ int servicesCount = builder.Services.Count;
 var logServices = builder.Services.Where(x => x.ServiceType.Name.Contains("Log")).ToArray();
 
 // Add services to the container.
-builder.Services.AddSingleton<IClient>(_ => {
+builder.Services.AddSingleton<IClient>(_ =>
+{
+    bool isLoad = bool.Parse(builder.Configuration["LoadTest:IsActive"]);
+    if (isLoad) return new ClientStub();
+    
     string apiKey = builder.Configuration["OpenWeather:Key"];
     HttpClient httpClient = new HttpClient();
     return new Client(apiKey, httpClient);
 });
 builder.Services.AddSingleton<INowWrapper>(_ => new NowWrapper());
 builder.Services.AddTransient<IRandomWrapper>(_ => new RandomWrapper());
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
