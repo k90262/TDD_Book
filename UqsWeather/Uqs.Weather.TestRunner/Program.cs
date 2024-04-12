09dd97734c4fea7e73ddaba4f57302d50f15aaa0
Bill Ho <ycho.bill.lab@gmail.com>
2024-04-12 18:27:16 +0800

    test DI and seam (e.g. LogInformation()) via console project TestRunner

---
+++ UqsWeather/Uqs.Weather.TestRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Uqs.Weather;
+using Uqs.Weather.Controllers;
+
+var logger = NullLogger<WeatherForecastController>.Instance;
+var controller = new WeatherForecastController(logger, null!);
+double f1 = controller.ConvertCToF(-1.0);
+if (f1 != 30.20d) throw new Exception("Invalid");
+double f2 = controller.ConvertCToF(1.2);
+if (f2 != 34.16d) throw new Exception("Invalid");
+Console.WriteLine("Test Passed");
