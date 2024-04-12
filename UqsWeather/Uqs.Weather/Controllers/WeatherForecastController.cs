22aea526c90411908a503eacd2255b0b9db493c8
Bill Ho <ycho.bill.lab@gmail.com>
2024-04-12 23:49:39 +0800

    refactor: changed to DI-ready code

--- UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
+++ UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
@@ -1,5 +1,6 @@
 using AdamTibi.OpenWeather;
 using Microsoft.AspNetCore.Mvc;
+using Uqs.Weather.Wrappers;
 
 namespace Uqs.Weather.Controllers;
 
@@ -10,6 +11,9 @@ public class WeatherForecastController : ControllerBase
     private const int FORECAST_DAYS = 5;
     private readonly ILogger<WeatherForecastController> _logger;
     private readonly IConfiguration _config;
+    private readonly IClient _client;
+    private readonly INowWrapper _nowWrapper;
+    private readonly IRandomWrapper _randomWrapper;
     
     private static readonly string[] Summaries = new[]
     {
@@ -24,10 +28,19 @@ public class WeatherForecastController : ControllerBase
         return Summaries[summariesIndex];
     }
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration config)
+    public WeatherForecastController(
+        ILogger<WeatherForecastController> logger, 
+        IConfiguration config, 
+        IClient client,
+        INowWrapper nowWrapper,
+        IRandomWrapper randomWrapper
+        )
     {
         _logger = logger;
         _config = config;
+        _client = client;
+        _nowWrapper = nowWrapper;
+        _randomWrapper = randomWrapper;
     }
 
     [HttpGet("ConvertCToF")]
@@ -43,10 +56,10 @@ public class WeatherForecastController : ControllerBase
     {
         return Enumerable.Range(1, FORECAST_DAYS).Select(index =>
             {
-                var tempC = Random.Shared.Next(-20, 55);
+                var tempC = _randomWrapper.Next(-20, 55);
                 return new WeatherForecast
                 {
-                    Date = DateTime.Now.AddDays(index),
+                    Date = _nowWrapper.Now.AddDays(index),
                     TemperatureC = tempC,
                     Summary = MapFeelToTemp(tempC)
                 };
@@ -60,10 +73,7 @@ public class WeatherForecastController : ControllerBase
     {
         const decimal GREENWICH_LAT = 51.4810m;
         const decimal GREENWICH_LON = 0.0052m;
-        string apiKey = _config["OpenWeather:Key"];
-        HttpClient httpClient = new HttpClient();
-        Client openWeatherClient = new Client(apiKey, httpClient);
-        OneCallResponse res = await openWeatherClient.OneCallAsync(GREENWICH_LAT, GREENWICH_LON, new []
+        OneCallResponse res = await _client.OneCallAsync(GREENWICH_LAT, GREENWICH_LON, new []
         {
             Excludes.Current, Excludes.Minutely, Excludes.Hourly, Excludes.Alerts
         }, Units.Metric);
--- UqsWeather/Uqs.Weather/Program.cs
+++ UqsWeather/Uqs.Weather/Program.cs
@@ -1,7 +1,19 @@
+using AdamTibi.OpenWeather;
+using Uqs.Weather.Wrappers;
+
 var builder = WebApplication.CreateBuilder(args);
+// List how many registered services has been created.
+int servicesCount = builder.Services.Count;
+var logServices = builder.Services.Where(x => x.ServiceType.Name.Contains("Log")).ToArray();
 
 // Add services to the container.
-
+builder.Services.AddSingleton<IClient>(_ => {
+    string apiKey = builder.Configuration["OpenWeather:Key"];
+    HttpClient httpClient = new HttpClient();
+    return new Client(apiKey, httpClient);
+});
+builder.Services.AddSingleton<INowWrapper>(_ => new NowWrapper());
+builder.Services.AddTransient<IRandomWrapper>(_ => new RandomWrapper());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
